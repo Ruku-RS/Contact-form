fa0e1cba90549bdc0078a7e600eeb3f4fa1795da
Rukman <rukmansubedi@gmail.com>
2025-08-05 17:43:55 +0545

    Contact form setup , used EmailJs to redirect the message to intented user

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from '@emailjs/browser'
 
 const ContactForm = () => {
+
+const formData = useRef('');
+
+const sendEmail = (e)=>{
+    e.preventDefault();
+
+    // Email setup
+    emailjs.sendForm('service_l4n9zqb', 'template_50wvi88', formData.current, 'r0MzDsaymRwexDK_-').then(
+        ()=>{
+            alert('Message sent successfully!');
+            formData.current.reset();
+        },
+        (error)=>{
+            alert('Failed to send message!');
+            console.error(error.text);
+        }
+    )
+}
+
   return (
     <main className="bg-white p-7 w-[600px] border-1 border-zinc-300 rounded-lg shadow-lg">
       {/* Form Heading */}
@@ -12,7 +32,7 @@ const ContactForm = () => {
       </div>
 
       {/* Form */}
-      <form autoComplete="off">
+      <form autoComplete="off" ref={formData} onSubmit={sendEmail}>
         <div className="flex items-center gap-5">
           <div className="flex-1">
             <label htmlFor="user-name" className="text-zinc-800">
@@ -23,6 +43,7 @@ const ContactForm = () => {
               name="user-name"
               id="user-name"
               placeholder="Name"
+              required
               className="border-1 border-zinc-300 w-full bg-zinc-100 h-11 pl-3 mt-2 text-sm focus:outline-blue-600 rounded-lg"
             />
           </div>
@@ -35,6 +56,7 @@ const ContactForm = () => {
               name="user-email"
               id="user-email"
               placeholder="Email"
+              required
               className="border-1 border-zinc-300 w-full bg-zinc-100 h-11 pl-3 mt-2 text-sm focus:outline-blue-600 rounded-lg"
             />
           </div>
@@ -50,13 +72,14 @@ const ContactForm = () => {
             name="user-subject"
             id="user-subject"
             placeholder="Subject"
+            required
             className="border-1 border-zinc-300 w-full bg-zinc-100 h-11 pl-3 mt-2 text-sm focus:outline-blue-600 rounded-lg"
           />
         </div>
 
         {/* Message Field */}
         <div className="mt-3">
-          <label htmlFor="user-message" className="text-zinc-800">
+          <label htmlFor="message" className="text-zinc-800">
             Message
           </label>
           <textarea
@@ -64,13 +87,14 @@ const ContactForm = () => {
             id="message"
             placeholder="Type your message here...."
             rows={5}
+            required
             className="border-1 border-zinc-300 w-full bg-zinc-100  p-3 mt-2 text-sm focus:outline-blue-600 rounded-lg resize-none"
           ></textarea>
         </div>
 
         {/* Submit */}
         <div className="mt-8">
-          <button type="submit" className="w-full py-3 bg-blue-600 text-white rounded-lg active:bg-blue-700">
+          <button type="submit" className="w-full py-3 bg-blue-600 text-white rounded-lg active:bg-blue-700 cursor-pointer">
             Send message
           </button>
         </div>
